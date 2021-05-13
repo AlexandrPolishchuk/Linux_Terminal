aad8d8f47d5c1a40947cedd23af5d33faf9994ef
Alexandr Polishchuk <alexandrp18@gmail.com>
2021-05-13 11:15:11 +0300

    made some changes

--- script.sh
+++ script.sh
@@ -1,9 +1,9 @@
 #!/bin/bash
 
 cd firstscript1/ # заходим в папку
-mkdir -p ~/folder_1 folder_2 folder_3 # создаем 3 папки
+mkdir folder_1 folder_2 folder_3 # создаем 3 папки
 cd folder_3 # заходим в папку
 touch file1.txt file2.txt file3.txt cell1.json cell2.json # создаем 5 файлов
-mkdir -p ~/sinus1 sinus2 sinus3 # создаем 3 папки
+mkdir sinus1 sinus2 sinus3 # создаем 3 папки
 ls -l # выводим содержимое папки
-mv /c/Users/Александра/group_18/git_repo/Linux_Terminal/firstscript1/folder_2/file3.txt /c/Users/Александра/group_18/git_repo/Linux_Terminal/firstscript1/folder_2/cell2.json folder_1 # переместим 2 файла в любую другую папку
+mv /c/Users/Александра/group_18/git_repo/Linux_Terminal/firstscript1/folder_3/file3.txt /c/Users/Александра/group_18/git_repo/Linux_Terminal/firstscript1/folder_3/cell2.json folder_2 # переместим 2 файла в любую другую папку
